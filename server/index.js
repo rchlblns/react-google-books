6ad23361816ceadc1bda53e6a1c0cdc847c5111f
Short Stack <41342213+rchlblns@users.noreply.github.com>
2020-11-27 11:00:03 -0600

    added cors and build scripts

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 // test route to make sure that we can reach the backend
 // will move to routes file after
 app.get("/test", (req, res) => {
